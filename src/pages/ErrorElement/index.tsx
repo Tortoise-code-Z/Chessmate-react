3b3c482cb742843c9388639a2b340a4f93c03a05
Victor Perez <tortoise.code@protonmail.com>
2025-08-16 16:37:32 +0100

    ErrorElement page refactorizado al completo

--- src/pages/ErrorElement/index.tsx
+++ src/pages/ErrorElement/index.tsx
@@ -1,7 +1,39 @@
-type Props = {};
+import { useRouteError, isRouteErrorResponse, NavLink } from "react-router-dom";
+import { paths } from "../../consts/paths";
+import styles from "./ErrorElement.module.css";
+import LightComponent from "../../components/LightComponent";
 
-function ErrorElement({}: Props) {
-    return <div>ErrorElement</div>;
+export default function ErrorElement() {
+    const error = useRouteError();
+
+    let title = "¡Ups! Algo salió mal";
+    let message = "Ocurrió un error inesperado.";
+
+    if (isRouteErrorResponse(error)) {
+        if (error.status === 404) {
+            title = "404 - Página no encontrada";
+            message = "La página que buscas no existe.";
+        } else {
+            title = `Error ${error.status}`;
+            message = error.statusText;
+        }
+    } else if (error instanceof Error) {
+        message = error.message;
+    }
+
+    return (
+        <section className={[styles.errorElement].join(" ")}>
+            <LightComponent top={20} right={75} />
+            <LightComponent top={80} right={25} />
+            <h1>{title}</h1>
+            <p className={[styles.message].join(" ")}>{message}</p>
+
+            <NavLink
+                to={`${paths.index}`}
+                className={["button", "buttonSecondary"].join(" ")}
+            >
+                Volver al inicio
+            </NavLink>
+        </section>
+    );
 }
-
-export default ErrorElement;
